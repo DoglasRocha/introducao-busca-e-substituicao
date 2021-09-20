a9d207c5f38359edc8cf5ab9473d565de4afc16e
DoglasRocha <doglas_rocha@protonmail.ch>
2021-09-20 00:02:25 -0300

    first challenge

--- challenge1.js
+++ challenge1.js
@@ -1,94 +1,41 @@
-// sort alphabetically the words with equal length
+// esta solução funciona na DIO, sendo necessário substituir o "prompt" pelo "gets"
+
+const regex = new RegExp('^[a-zA-Z0-9 ]{1,50}$i');
 
 const getNumberOfLines = () => {
-  var amountOfLines = parseInt(prompt('entre o numero de linhas'));
-
-  if (amountOfLines < 1 && amountOfLines > 50) {
-    return getNumberOfLines();
-  }
-  
-  return amountOfLines;
+	var amountOfLines = parseInt(prompt('entre o numero de linhas');
+  	return amountOfLines;
 }
 
 const getLine = () => {
-  var line = prompt('escreva as palavras');
-  
-  if (1 < line.length && line.length > 50) {
-    return getLine();
-  }
-  
-  return line;
+ 	var line = prompt('escreva as palavras');
+  	return line;
 }
 
 const getLines = numberOfLines => {
-  let lines = [];
+  	let lines = [];
+
+ 	 for(let i=0; i < numberOfLines; i++) {
+    	lines.push(getLine());
+  	}
   
-  for(let i=0; i < numberOfLines; i++) {
-    lines.push(getLine());
-  }
-  
-  return lines;
+  	return lines;
 }
 
-const orderArrayByLength = line => {
-  let separatedLine = line.split(' ');
-  let sortedWordsByLength = [];
-  
-  for(let word of separatedLine) {
-      
-    if (sortedWordsByLength.length <= 0) {
-      sortedWordsByLength.push(word);
-        
-    } else {
-      sortedWordsByLength.forEach((item, index, array) => {
-        if (word.length > item.length && !sortedWordsByLength.includes(word)) {
-          sortedWordsByLength.splice(index, 0, word);
-        }
-        
-        if (!sortedWordsByLength.includes(word) && index === sortedWordsByLength.length - 1) {
-          sortedWordsByLength.push(word);
-        }
-      });
-    }
-  }
-  
-  return sortedWordsByLength.join(' ');
-}
+const orderLines = array => {
+	for(let line of array) {
 
-const orderArrayAlphabetically = sortedWordsByLength => {
-  let separatedLine = sortedWordsByLength.split(' ');
-  let lengths = [];
-  
-  separatedLine.forEach((item) => lengths.push(item.length));
-  let nonRepeatedLengths = [...new Set(lengths)];
-  let sortedAlphabeticallyWords = [];
-  
-  nonRepeatedLengths.forEach((length) => {
-    let sortedItem = separatedLine.filter(item => item.length === length);
-	sortedItem.sort();
-    sortedAlphabeticallyWords = sortedAlphabeticallyWords.concat(sortedItem);
-	debugger;
-  });
-  return sortedAlphabeticallyWords.join(' ');
-  
-}
+		splittedLine = line.split(' ', 50);
+		filteredLine = splittedLine.filter((word) => regex.test(word));
+		filteredLine.sort();
+		filteredLine.sort((current, next) => next.length - current.length);
 
-const orderArray = arrayOfLines => {
-  let newArray = [];
-  
-  for(let i=0; i < arrayOfLines.length; i++) {
-    let line = arrayOfLines[i];
-    let sortedWordsByLength = orderArrayByLength(line);
-    let sortedWordsByLengthAndAlphabetically = orderArrayAlphabetically(sortedWordsByLength);
-    newArray.push(sortedWordsByLengthAndAlphabetically);
-    
-  }
-  
-  return newArray;
+		let orderedLine = filteredLine.join(' ');
+		console.log(orderedLine);
+	}
 }
 
 let numberOfLines = getNumberOfLines();
 let lines = getLines(numberOfLines);
-let sortedLines = orderArray(lines);
+orderLines(lines);
 
-sortedLines.forEach(item => console.log(item));
